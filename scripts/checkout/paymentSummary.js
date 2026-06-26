8f049f50018bef56a829b20d07399f13a8c95f3a
Fardin <fardintorkamand83@gmail.com>
2026-06-26 16:36:42 +0330

    Make items interactive in paymetSummary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import { cart } from "../../data/cart.js";
+import { cart, getCartQuantity } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct } from "../../data/products.js";
 import formatCurrency from "../utils/money.js";
@@ -20,7 +20,7 @@ export function renderPaymentSummary() {
         <div class="payment-summary-title">Order Summary</div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${getCartQuantity()}):</div>
             <div class="payment-summary-money">
             $${formatCurrency(productPriceCents)}
             </div>
