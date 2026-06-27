61b0abddcf34756675dc9ec048f61437e0ad2b1a
Fardin <fardintorkamand83@gmail.com>
2026-06-27 18:23:41 +0330

    Implement weekend filtering for delivery date and fix module import

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,17 +1,17 @@
-
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 export const deliveryOptions = [
   {
-    id: '1',
+    id: "1",
     deliveryDays: 7,
     priceCents: 0,
   },
   {
-    id: '2',
+    id: "2",
     deliveryDays: 3,
     priceCents: 499,
   },
   {
-    id: '3',
+    id: "3",
     deliveryDays: 1,
     priceCents: 999,
   },
@@ -24,12 +24,26 @@ export function getDeliveryOption(deliveryOptionId) {
       matchingOption = option;
     }
   });
-  return matchingOption || deliveryOptions[0]
+  return matchingOption || deliveryOptions[0];
+}
+
+function isWeekend(date) {
+  const dayOfWeek = date.format("dddd");
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
 }
 
 export function calculateDeliveryDateString(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, "day");
+
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
+
   const dateString = deliveryDate.format("dddd, MMMM D");
   return dateString;
 }
