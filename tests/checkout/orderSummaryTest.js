93278493190888435834f9536b2cd17d529e0b70
Fardin <fardintorkamand83@gmail.com>
2026-07-02 16:15:01 +0330

    Check if the product name is displayed correctly on the page

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -41,7 +41,14 @@ describe("test suit: renderOrderSummary", () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText,
     ).toContain("Quantity: 1");
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText,
+    ).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs");
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual("Intermediate Size Basketball");
   });
+
   it("removes a product", () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
     expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
@@ -55,5 +62,8 @@ describe("test suit: renderOrderSummary", () => {
     ).not.toEqual(null);
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText,
+    ).toEqual("Intermediate Size Basketball");
   });
 });
